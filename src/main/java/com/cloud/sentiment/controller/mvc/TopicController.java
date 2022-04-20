e6081c48bedc55ef7affa87d45549b9af6ca5531
Garric Mathias <garricmathias@gmail.com>
2022-04-20 03:09:14 -0400

    Updated Controller

--- src/main/java/com/cloud/sentiment/controller/mvc/TopicController.java
+++ src/main/java/com/cloud/sentiment/controller/mvc/TopicController.java
@@ -1,26 +1,53 @@
 package com.cloud.sentiment.controller.mvc;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cloud.sentiment.constant.TopicStatus;
+import com.cloud.sentiment.model.Topic;
+import com.cloud.sentiment.service.TopicService;
 
 @Controller
 public class TopicController {
 
-	@RequestMapping("/")
+	@Autowired
+	private TopicService topicService;
+
+	@GetMapping("/")
 	public String index(Model model) {
 		return "index";
 	}
 
-	@RequestMapping("/add-topic")
+	@GetMapping("/add-topic")
 	public String addTopic(Model model) {
+		model.addAttribute("topic", new Topic());
 		return "add-topic";
 	}
-	
-	@RequestMapping("/view-topic")
+
+	@PostMapping("/create-topic")
+	public String postTopic(Topic topic, BindingResult result, Model model) {
+		topic.setStatus(TopicStatus.STATUS_IN_PROGRESS);
+		topicService.insert(topic);
+		return "redirect:/view-topic";
+	}
+
+	@GetMapping("/view-topic")
 	public String viewTopic(Model model) {
-//		model.addAttribute("name", name);
+		List<Topic> topicList = topicService.getTopics();
+		model.addAttribute("topicList", topicList);
+
 		return "view-topic";
+	}
+
+	@GetMapping("/word-cloud")
+	public String viewWordCloud(Model model) {
+		return "word-cloud";
 	}
 
 }
